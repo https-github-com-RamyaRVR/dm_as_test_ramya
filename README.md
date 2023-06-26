3d37577a5b7259114e2dcc3f43f4787290fe89be
ramya <ramya@LAPTOP-L0458MLL>
2023-06-26 14:04:40 +0530

    Add project files.

---
+++ README.md
@@ -0,0 +1,1 @@
+# dm_as_test_ramya
---
+++ dm_as_test_ramya_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dm_as_test_ramya_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("dm_as_test_ramya_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("638F5475-EEB8-4FCD-AFD7-8BCD770FE435")]
+[assembly: AssemblyVersion("1.0.0.0")]
